6f48db391f3352f2d780e5790b45161ad1e8d2f7
vinceye <vinceye.cm@gmail.com>
2022-11-28 11:51:39 -0800

    lesson 2 repeat string complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, numReps) {
+    if (numReps < 0) {
+        return 'ERROR';
+    }
+    let finalString = '';
+    for (let i = 0; i < numReps; i++) {
+        finalString += inputString;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
