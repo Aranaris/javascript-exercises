893f9f22e7c1a1bb2656a9dff015eb192324e361
vinceye <vinceye.cm@gmail.com>
2022-12-01 11:22:09 -0800

    lesson 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
-};
+const reverseString = function(phrase) {
+    newString = '';
+    if (phrase.length >= 0) {
+        for (i = 0; i <= phrase.length; i++) {
+            newString += phrase.charAt(phrase.length - i);
+        } 
+    } else {
+            return 'ERROR';
+        }
+    return newString;
+}
+;
 
 // Do not edit below this line
 module.exports = reverseString;
