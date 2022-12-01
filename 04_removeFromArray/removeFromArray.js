11ef1fd33767f9e07681bbae92bd576eff773ece
vinceye <vinceye.cm@gmail.com>
2022-12-01 11:43:05 -0800

    completed lesson 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(input, ...args) {
+    for (i = 0; i < input.length; i++) {
+        for (j = 0; j < args.length; j++) {
+            if (input[i] === args[j]) {
+                input.splice(i, 1);
+                i -= 1;
+            }    
+        }
+    }
+    console.log(input);
+    return input;
 };
 
 // Do not edit below this line
