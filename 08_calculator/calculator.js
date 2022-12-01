79656fbc0aed0fd7892fa70976c2cde6711a8282
vinceye <vinceye.cm@gmail.com>
2022-12-01 12:28:32 -0800

    finished lesson 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+	let output = 0;
+  for (let i = 0; i < input.length; i++) {
+    output += input[i];
+  }
+  return output;
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+  let output = 1;
+  for (let i = 0; i < input.length; i++) {
+    output *= input[i];
+  }
+  return output;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let output = 1;
+  if (a === 0) {
+    return 1;
+  } else if (a > 0) {
+    for (let i = 1; i <= a; i++) {
+      output *= i;
+    }
+  } else {
+    return 'ERROR';
+  }
+  return output;
 };
 
 // Do not edit below this line
