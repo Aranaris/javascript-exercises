dda92c569131e3c18c203c3b2a5fa783401c8142
vinceye <vinceye.cm@gmail.com>
2022-12-01 16:24:43 -0800

    completed lesson 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let reverseInput = '';
+    let strippedInput = '';
+    for (let i = 0; i < input.length; i++) {
+        if (/^[A-Za-z]+$/.test(input.charAt(i)) ) {
+            strippedInput += input.charAt(i);
+            reverseInput = input.charAt(i) + reverseInput;
+        }
+    }
+    if (reverseInput.toUpperCase() === strippedInput.toUpperCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
