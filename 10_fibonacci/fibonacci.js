f1305d288a006b45727dc5ca5f859a7d3a4afe49
vinceye <vinceye.cm@gmail.com>
2022-12-02 12:58:33 -0800

    completed lesson 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let output = 1;
+    let input = parseInt(num);
+    if (input <= 0) {
+        return "OOPS";
+    } else if (input <= 2) {
+        return output;
+    } else {
+        return fibonacci(input - 2) + fibonacci(input - 1);
+    }
 };
 
 // Do not edit below this line
