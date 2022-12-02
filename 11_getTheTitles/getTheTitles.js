e89a73c9fb802d8dc226f52be3744a0537950a40
vinceye <vinceye.cm@gmail.com>
2022-12-02 14:00:19 -0800

    finished lesson 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(inputArray) {
+    let titles = [];
+    for (let i = 0; i < inputArray.length; i++) {
+        titles.push(inputArray[i].title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
