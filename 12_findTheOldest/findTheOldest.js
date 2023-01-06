802557dbd3bf0c2494a5fb98e57ecd43b89254d9
vinceye <vinceye.cm@gmail.com>
2023-01-06 10:26:11 -0800

    finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = 2023;
+    let maxAge = 0;
+    let oldest = {};
+    for(let i = 0; i < people.length; i++) {
+        if (people[i]["yearOfDeath"]) {
+            people[i]["age"] = people[i]["yearOfDeath"] - people[i]["yearOfBirth"];
+        } else {
+            people[i]["age"] = currentYear - people[i]["yearOfBirth"];
+        };
+        if (people[i]["age"] > maxAge) {
+            maxAge = people[i]["age"];
+            oldest = people[i];
+        };
+    };
+    return oldest;
 };
 
 // Do not edit below this line
